1bb25f0bc890c1345cc5a206b80c7b81d2d8c9d0
Shencko <eajimati028@gmail.com>
2024-05-20 16:25:16 +0200

    Fix Preact build issue and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-  build: {
-    rollupOptions: {
-      external: ["preact"],
-      base: "/ci-cd-testing/",
-    },
-  },
+  plugins: [react()],
+  base: "/ci-cd-testing/",
 });
